2bd834ec4d95552873d183b0761fec733cd6fcd9
OstarkovVictor <119045520+OstarkovVictor@users.noreply.github.com>
2024-06-13 07:46:56 +0500

    1

---
+++ Лабораторная 1/Number_in_power.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std; 
+
+void exponention()
+{
+	int i, k, num;
+	cout << "Введите основание для вычисления степени: ";
+	cin >> num;
+	int power(int, int);
+	for (i = 0; i < 10; i++)
+	{
+		k=power(num, i);
+		cout<< "Число: "<< num << "\tстепень: " << i << "\tитог: " << k << endl;
+	}
+}
+
+int power(int x, int n)
+{
+	int i, p = 1;
+	for (i = 1; i < n; i++) p *= x;
+	return p;
+}
+
+int main(){
+	exponention();
+	
+}
